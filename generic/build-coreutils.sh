17260dff26d684e5912a31707a267e65852e4f94
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-11-16 12:24:56 -0800

    default to .tar.xz urls for coreutils

--- generic/build-coreutils.sh
+++ generic/build-coreutils.sh
@@ -4,6 +4,9 @@
 
 export LATEST_VERSION='8.32'
 
+# NB: More versions of coreutils are available at .tar.xz URLs for some reason?
+export ARCHIVE_URL_EXTENSION="${ARCHIVE_URL_EXTENSION:-.tar.xz}"
+
 exec generic/build-configure-helper.sh \
      coreutils \
      "${1:-latest}" \
